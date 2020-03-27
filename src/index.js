6746449c7b99cc403cfbbf47046d481ec6bca220
Andrey Manakhov <andreibikerrr@gmail.com>
2020-03-27 09:36:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 
 exports.cToF = function (c) {
-
+    let f = c * 9 / 5 + 32;
+    return f.toFixed(2);
 }
 
 exports.fToC = function (f) {
-
+    let c = (f - 32) * 5 / 9;
+    return c.toFixed(2);
 }
